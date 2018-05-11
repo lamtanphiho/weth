874db1dd69f820fa0147996b8840e7810fedb760
taitt123 <tantai@bacoor.co>
2018-05-11 10:28:55 +0700

    update readme file

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # weth
 call w-eth's smart contract 
+
+
+## Super simple to use
+
+Require and config: 
+
+```js
+const Weth = require('weth');
+Weth.config({
+    web3: web3, // your web3,
+    contractAddress: contractAddress// weth's contract address
+});
+
+```
+
+## withdraw
+
+UNWRAP WETH to ETH: 
+
+```js
+Weth.withdraw(address, privateKeyAddress, amount);
+```
+
+## deposit
+
+WRAP ETH to WETH: 
+
+```js
+Weth.deposit(address, privateKeyAddress, amount);
+```
--- index.js
+++ index.js
@@ -1,62 +1,54 @@
+const ethTx             = require('ethereumjs-tx');
 let Web3;
+let contractAddress;
 module.exports = {
-    config: (web3) => {
-        Web3 = web3;
+    config: (config) => {
+        Web3 = config.web3;
+        contractAddress = config.contractAddress;
     },
     withdraw: (from, privateKeyUser, amount) => {
         let rawTransaction;
-        const amountTransfer = amount * 1e18;
-        const privKey         = Buffer.from(privateKeyUser, 'hex');
+        const amountTransfer    = amount * 1e18;
+        const amountTransferHex = '0x' + amountTransfer.toString(16);
+        const privKey           = Buffer.from(privateKeyUser, 'hex');
         return Web3.eth.getTransactionCount(from).then(nonce => {
             rawTransaction = {
                 'from'    : from,
                 'nonce'   : '0x' + nonce.toString(16),
-                'gasPrice': Web3.utils.toHex(3 * 1e9),
+                'gasPrice': Web3.utils.toHex(2 * 1e9),
                 'gasLimit': Web3.utils.toHex(3000000),
                 'to'      : contractAddress,
-                'data'    : contract.methods.withdraw('0x' + amountTransfer.toString(16)).encodeABI()
+                'data'    : contract.methods.withdraw(amountTransferHex).encodeABI()
             };
-        const tx                    = new ethTx(rawTransaction);
-        tx.sign(privKey);
-        const serializedTx          = tx.serialize();
-        return Web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex')).on('transactionHash', (transactionHash) => {
-            console.info(transactionHash);
-        }).on('receipt', (receipt) => {
-                console.info(receipt);
-        }).on('error', (err) => {
-                console.info(err);
-        });
-        }).catch(err => {
-                console.log(err);
+            const tx                    = new ethTx(rawTransaction);
+            tx.sign(privKey);
+            const serializedTx          = tx.serialize();
+            return Web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex')).on('transactionHash', (transactionHash) => {
+                console.info('transactionHash: ', transactionHash);
+            });
         });
     },
 
     deposit: (from, privateKeyUser, amount) => {
         let rawTransaction;
         const amountTransfer = amount * 1e18;
-        console.log(from);
+        const amountTransferHex = '0x' + amountTransfer.toString(16);
         return Web3.eth.getTransactionCount(from).then(nonce => {
             rawTransaction = {
                 'from'    : from,
                 'nonce'   : '0x' + nonce.toString(16),
-                'gasPrice': Web3.utils.toHex(3 * 1e9),
+                'gasPrice': Web3.utils.toHex(2 * 1e9),
                 'gasLimit': Web3.utils.toHex(3000000),
                 'to'      : contractAddress,
-                'value'   : '0x' + amountTransfer.toString(16),
+                'value'   : amountTransferHex,
                 'data'    : contract.methods.deposit().encodeABI()
             };
-        const tx                    = new ethTx(rawTransaction);
-        tx.sign(privKey);
-        const serializedTx          = tx.serialize();
-        return Web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex')).on('transactionHash', (transactionHash) => {
-            console.info(transactionHash);
-        }).on('receipt', (receipt) => {
-                console.info(receipt);
-        }).on('error', (err) => {
-                console.info(err);
-        });
-        }).catch(err => {
-                console.log(err);
+            const tx                    = new ethTx(rawTransaction);
+            tx.sign(privKey);
+            const serializedTx          = tx.serialize();
+            return Web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex')).on('transactionHash', (transactionHash) => {
+                console.info('transactionHash: ', transactionHash);
+            });
         });
     }
 }
